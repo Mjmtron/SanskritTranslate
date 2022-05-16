2eeacd4ba1ba06fad71e84d8b83e05f48aa17a95
Mjmtron <95286009+Mjmtron@users.noreply.github.com>
2022-05-16 17:04:44 +0530

    Add files via upload

---
+++ sanskrit-translator.py
@@ -0,0 +1,291 @@
+class sanskrit:
+	def vocabulary(a):
+		if a.lower()=="hello":
+			print("namaste",end=" ")
+		elif a.lower()=="and":
+			print("एवम्",end=" ")
+		elif a.lower()=="answer":
+			print("उत्तरम्",end=" ")
+		elif a.lower()=="am":
+			print("",end="")
+		elif a.lower()=="after"or a.lower()=="afterward":
+			print("पश्चात्",end="")
+		elif a.lower()=="animal":
+			print("पशु",end=" ")
+		elif a.lower()=="all"or a.lower()=="everything":
+			print("सर्व",end=" ")
+		elif a.lower()=="bad":
+			print("अघ",end=" ")
+		elif a.lower()=="boy":
+			print("बालका",end=" ")
+		elif a.lower()=="big":
+			print("विशाल",end=" ")
+		elif a.lower()=="bro"or a.lower()=="brother":
+			print("भ्राता",end=" ")
+		elif a.lower()=="body":
+			print("शरीर",end=" ")
+		elif a.lower()=="book":
+			print("पुस्तकम्",end=" ")
+		elif a.lower()=="books"or a.lower()=="book's":
+			print("पुस्तके",end=" ")
+		elif a.lower()=="best":
+			print("श्रेष्ठ",end=" ")
+		elif a.lower()=="beast":
+			print("हिंसक पशु",end=" ")
+		elif a.lower()=="blue":
+			print("नील",end=" ")
+		elif a.lower()=="birthday":
+			print("जन्मदिन:",end=" ")
+		elif a.lower()=="blue":
+			print("नीलः",end=" ")
+		elif a.lower()=="black":
+			print("कृष्णः",end=" ")
+		elif a.lower()=="birth" or a.lower()=="born":
+			print("जन्मा:",end=" ")
+		elif a.lower()=="brown":
+			print("कपिशः",end=" ")
+		elif a.lower()=="bitch":
+			print("श्वानी",end=" ")
+		elif a.lower()=="cat":
+			print("बिलाव",end=" ")
+		elif a.lower()=="call":
+			print("आव्हे",end=" ")
+		elif a.lower()=="calling":
+			print("अवहायमी",end=" ")
+		elif a.lower()=="color" or a.lower()=="colour":
+			print("रंग",end=" ")
+		elif a.lower()=="colorful"or a.lower()=="colourful":
+			print("रंगीन",end=" ")
+		elif a.lower()=="clan":
+			print("कुल",end=" ")
+		elif a.lower()=="clean":
+			print("स्वच्छ",end=" ")
+		elif a.lower()=="construction":
+			print("निर्माण",end=" ")
+		elif a.lower()=="constructing":
+			print("निर्माण करोति",end=" ")
+		elif a.lower()=="day":
+			print("दिन:",end=" ")
+		elif a.lower()=="dad" or a.lower()=="father" or a.lower()=="papa":
+			print("पिता जी",end=" ")
+		elif a.lower()=="death":
+			print("मृत्यु",end=" ")
+		elif a.lower()=="dead":
+			print("मृत",end=" ")
+		elif a.lower()=="dirty":
+			print("मलिन",end=" ")
+		elif a.lower()=="do":
+			print("कृ",end=" ")
+		elif a.lower()=="dog":
+			print("श्वान:",end=" ")
+		elif a.lower()=="east":
+			print("पूर्वा",end=" ")
+		elif a.lower()=="even":
+			print("अपि",end=" ")
+		elif a.lower()=="elder":
+			print("ज्येष्ठा",end=" ")
+		elif a.lower()=="elephant":
+			print("गज:",end=" ")	
+		elif a.lower()=="earn":
+			print("अर्जयति",end=" ")
+		elif a.lower()=="eat":
+			print("भक्षणं",end=" ")
+		elif a.lower()=="eater":
+			print("भक्षी",end=" ")
+		elif a.lower()=="eight"or a.lower()=="8":
+			print("अष्ट",end=" ")
+		elif a.lower()=="fruit":
+			print("फलम्",end=" ")
+		elif a.lower()=="fruitfull":
+			print("मधुफलम्",end=" ")
+		elif a.lower()=="free":
+			print("मुक्ता:",end=" ")
+		elif a.lower()=="fine":
+			print("कुशल",end=" ")
+		elif a.lower()=="four"or a.lower()=="4":
+			print("चत्वारः",end=" ")
+		elif a.lower()=="five"or a.lower()=="5":
+			print("पञ्च",end=" ")		
+		elif a.lower()=="girl":
+			print("बालिका",end=" ")
+		elif a.lower()=="green":
+			print("हरितः",end=" ")
+		elif a.lower()=="grey":
+			print("धूम्रवर्ण",end=" ")	
+		elif a.lower()=="good":
+			print("उत्तम",end=" ")
+		elif a.lower()=="go":
+			print("गच्छ:",end=" ")
+		elif a.lower()=="great":
+			print("महिमावान्",end=" ")
+		elif a.lower()=="goodmorning":
+			print("सुप्रभातम्",end=" ")	
+		elif a.lower()=="goodnight":
+			print("सुरात्रौ",end=" ")
+		elif a.lower()=="god":
+			print("देवं",end=" ")
+		elif a.lower()=="going":
+			print("गच्छति",end=" ")
+		elif a.lower()=="he"or a.lower()=="she"or a.lower()=="the":
+			print("सः",end=" ")
+		elif a.lower()=="how":
+			print("कथम",end=" ")
+		elif a.lower()=="house"or a.lower()=="home":
+			print("गृहम्",end=" ")
+		elif a.lower()=="i"or a.lower()=="i'm" or a.lower()=="im":
+			print("अहं",end=" ")
+		elif a.lower()=="impossible":
+			print("असंभव",end=" ")
+		elif a.lower()=="it"or a.lower()=="its"or a.lower()=="it's":
+			print("सः",end=" ")
+		elif a.lower()=="just":
+			print("मातरं",end=" ")
+		elif a.lower()=="king":
+			print("राजन् ",end=" ")
+		elif a.lower()=="love":
+			print("प्रेम:",end=" ")
+		elif a.lower()=="man":
+			print("पुरुषः",end=" ")
+		elif a.lower()=="maneater":
+			print("मनुष्यभक्षी",end=" ")
+		elif a.lower()=="man-eater":
+			print("नरभक्षी",end=" ")
+		elif a.lower()=="moron" or a.lower()=="stupid":
+			print("मूर्ख:",end=" ")
+		elif a.lower()=="mummy" or a.lower()=="mom" or a.lower()=="mum":
+			print("माता जी",end=" ")
+		elif a.lower()=="money":
+			print("धन",end=" ")
+		elif a.lower()=="my"or a.lower()=="me":
+			print("मम",end=" ")
+		elif a.lower()=="morning":
+			print("प्रभातम्",end=" ")	
+		elif a.lower()=="moon":
+			print("चन्द्र:",end=" ")
+		elif a.lower()=="much"or a.lower()=="more":
+			print("अतीव",end=" ")
+		elif a.lower()=="name":
+			print("नाम:",end=" ")
+		elif a.lower()=="nature":
+			print("प्रकृति",end=" ")
+		elif a.lower()=="night":
+			print("रात्रौ",end=" ")
+		elif a.lower()=="nine"or a.lower()=="9":
+			print("नव",end=" ")
+		elif a.lower()=="no":
+			print("ना",end=" ")
+		elif a.lower()=="north":
+			print("उत्तर",end=" ")
+		elif a.lower()=="or":
+			print("अथवा",end=" ")
+		elif a.lower()=="orange":
+			print("नारिङ्गः",end=" ")
+		elif a.lower()=="one"or a.lower()=="a" or a.lower()=="1":
+			print("एक:",end=" ")
+		elif a.lower()=="proof":
+			print("प्रमाण",end=" ")
+		elif a.lower()=="possible":
+			print("संभव",end=" ")
+		elif a.lower()=="possibility":
+			print("संभावना",end=" ")
+		elif a.lower()=="purple":
+			print("धूमलवर्णः",end=" ")
+		elif a.lower()=="pink":
+			print("पाटलः",end=" ")
+		elif a.lower()=="question":
+			print("प्रश्नम्",end=" ")
+		elif a.lower()=="red":
+			print("रक्त",end=" ")
+		elif a.lower()=="school":
+			print("विद्यालय",end=" ")
+		elif a.lower()=="seal"or a.lower()=="mark" or a.lower()=="gesture":
+			print("मोहर",end=" ")
+		elif a.lower()=="sun":
+			print("सूर्य",end=" ")
+		elif a.lower()=="sis"or a.lower()=="sister":
+			print("भगिनी",end=" ")
+		elif a.lower()=="six"or a.lower()=="6":
+			print("षट्",end=" ")
+		elif a.lower()=="seven"or a.lower()=="7":
+			print("सप्त",end=" ")
+		elif a.lower()=="south":
+			print("दक्षिण",end=" ")
+		elif a.lower()=="table":
+			print("उत्पीठिका",end=" ")
+		elif a.lower()=="tail":
+			print("पुच्छ:",end=" ")
+		elif a.lower()=="two"or a.lower()=="2":
+			print("द्वि",end=" ")
+		elif a.lower()=="three"or a.lower()=="3":
+			print("त्रयः",end=" ")
+		elif a.lower()=="this":
+			print("एतत्",end=" ")
+		elif a.lower()=="that":
+			print("तत्",end=" ")	
+		elif a.lower()=="to":
+			print("",end="")
+		elif a.lower()=="tomorrow":
+			print("श्वः",end=" ")
+		elif a.lower()=="very":
+			print("अति",end=" ")
+		elif a.lower()=="violence":
+			print("हिंसा",end=" ")
+		elif a.lower()=="will":
+			print("करोति",end=" ")
+		elif a.lower()=="woman":
+			print("युवती",end=" ")
+		elif a.lower()=="is"or a.lower()=="are" or a.lower()=="was"or a.lower()=="were":
+			print("अस्ति",end=" ")
+		elif a.lower()=="who":
+			print("किम्",end=" ")
+		elif a.lower()=="west":
+			print("दक्षिण",end=" ")
+		elif a.lower()=="work":
+			print("कार्यं",end=" ")				
+		elif a.lower()=="wait":
+			print("प्रतीक्षा",end=" ")
+		elif a.lower()=="where":
+			print("कुत्र",end=" ")
+		elif a.lower()=="while":
+			print("यस्य:",end=" ")
+		elif a.lower()=="work":
+			print("कार्य:",end=" ")		
+		elif a.lower()=="world":
+			print("लोक:",end=" ")
+		elif a.lower()=="when":
+			print("कदा",end=" ")
+		elif a.lower()=="white":
+			print("श्वेतः",end=" ")
+		elif a.lower()=="whenever":
+			print("यदा",end=" ")
+		elif a.lower()=="what":
+			print("किम्",end=" ")
+		elif a.lower()=="yes":
+			print("आम्",end=" ")
+		elif a.lower()=="yellow":
+			print("पीतः",end=" ")
+		elif a.lower()=="yesterday":
+			print("ह्यस्",end=" ")
+		elif a.lower()=="you"or a.lower()=="your" or a.lower()=="you're":
+			print("त्वं",end=" ")
+		elif a.lower()==".":
+			print("।",end=" ")			
+		else :
+			print(a,end=" ")	
+	def read():
+		ptr=0
+		w=input("enter sentence:\n")
+		word=w+" "+"#"
+		print("\nTRANSLATED TO SANSKRIT:\n")
+		while(word[ptr]!="#"):
+			a=""
+			while(word[ptr]!=" "):
+				a+=word[ptr]
+				ptr+=1	
+			sanskrit.vocabulary(a)
+			if ptr<=len(word):
+				ptr+=1
+		print("\n")
+
+for I in range(2):
+	sanskrit.read()	
